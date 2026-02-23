ca6ba86c73d45e5e8eb95e7b08d6813aef0545fd
Adryann <adryannofc@gmail.com>
2026-02-23 02:52:19 -0300

    ✨ adiciona atributo cardápio e método getCardapioService na classe Restaurante

--- src/main/java/com/pedidos/domain/Restaurante.java
+++ src/main/java/com/pedidos/domain/Restaurante.java
@@ -1,7 +1,18 @@
 package com.pedidos.domain;
 
+import com.pedidos.application.CardapioService;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Restaurante extends Usuario {
+    private List<Prato> cardapio;
     public Restaurante(String nome, String email, String senha) {
-        super(email, senha, nome);
+        super(nome, email, senha);
     }
+
+   public CardapioService getCardapioService() {
+        return new CardapioService();
+   }
+
 }
